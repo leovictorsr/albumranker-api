62c8d4dba4344a77222379329a4645955bb60b59
Leonardo Victor da Silva Rodrigues <vntlero@suportes-MacBook-Pro.local>
2020-06-01 23:59:12 -0300

    Remove old frontend handlers

--- src/js/config/database.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require("mongoose");
-
-module.exports = mongoose.connect("mongodb://localhost:27017/albumranker", {
-    useNewUrlParser: true
-});
--- src/js/services/item_service.js
+++
@@ -1,6 +0,0 @@
-const Ranking = require("../items/ranking");
-
-Ranking.methods(["get", "post", "put", "delete"]);
-Ranking.updateOptions({new: true, runValidators: true});
-
-module.exports = Ranking;
